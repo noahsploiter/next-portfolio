8318d8a361dac3e35876696b35025fad61ae1209
unknown <noahbeshahb@gmail.com>
2023-09-19 12:05:21 -0700

    'next'

--- apps/enji.dev/src/components/meta/Head.tsx
+++ apps/enji.dev/src/components/meta/Head.tsx
@@ -21,7 +21,7 @@ function Head({
 
   const htmlTitle = overrideTitle
     ? title
-    : `${title} — Enji · Front-End Developer`;
+    : `${title} — Noahsploit · Full-Stack Developer`;
 
   return (
     <NextHead>
