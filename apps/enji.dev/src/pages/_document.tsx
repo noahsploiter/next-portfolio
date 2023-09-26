dad76bd97d866fb55bca7be0f69c7e281ddb6c4f
unknown <noahbeshahb@gmail.com>
2023-09-26 01:25:46 -0700

    'seo'

--- apps/enji.dev/src/pages/_document.tsx
+++ apps/enji.dev/src/pages/_document.tsx
@@ -4,6 +4,10 @@ function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta
+        name="google-site-verification"
+        content="kGz09HkKafSCHB1lzH3_WUzZdVRozuwkCYZBtNCzQQY"
+      />
       <body>
         <div id="skip-navigation" />
         <Main />
